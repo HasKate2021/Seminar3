58a54471ef942c444e7a0e65f9c6d8e04161c382
Kit <simkatesi@yandex.ru>
2022-10-12 11:00:01 +0400

    задача 25

---
+++ 25/25.cs
@@ -0,0 +1,19 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+
+Console.Clear();
+Console.Write("Введите А: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите B: ");
+int b = int.Parse(Console.ReadLine()!);
+
+int NatSt (num1,num2)
+{
+    for(int i=0;i<num2;i++)
+    {
+        num*=num;
+    }
+    return num;
+}
+
+Console.WriteLine(NatSt(a,b));
