b9656e6b40b2adcf97edc6a7ab98a760883d796e
Kit <simkatesi@yandex.ru>
2022-10-12 11:39:29 +0400

    задача 27 решена

---
+++ 27/27.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int Sum (int n)
+{
+    int a1 = n % 10;
+    int a2 = n /10 % 10;
+    int a3 = n /100 % 10;
+    int a4 = n /1000;
+    n = a1+a2+a3+a4;
+    return n;
+}
+
+Console.WriteLine(Sum(N));
